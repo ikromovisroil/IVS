96c5e8f31eaf82ccac24f1803f431c2d5101718c
Ikromov Isroil <ikromovisroil0309@gmail.com>
2026-01-16 10:14:54 +0500

    1

--- main/views.py
+++ main/views.py
@@ -506,13 +506,19 @@ def barn_tex(request):
 
     if organization_id:
         qs = qs.filter(organization_id=organization_id)
-        employees = employees = (
-                Employee.objects
-                .select_related("organization")
-                .filter(organization_id=organization_id)
-                .only("id", "first_name", "last_name", "father_name")
-                .order_by("last_name", "first_name")
+        employees = (
+            Employee.objects
+            .select_related("user")
+            .filter(organization_id=organization_id)
+            .only(
+                "id",
+                "first_name",
+                "last_name",
+                "father_name",
+                "user__username",
             )
+            .order_by("last_name", "first_name")
+        )
     if status:
         qs = qs.filter(status=status)
     if category_id:
