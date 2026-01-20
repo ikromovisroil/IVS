b32aac3bc80125e0e019e60a08dd1ebfd9d0a950
Ikromov Isroil <ikromovisroil0309@gmail.com>
2026-01-20 15:37:58 +0500

    1

--- main/admin.py
+++ main/admin.py
@@ -13,7 +13,7 @@ admin.site.unregister(Group)
 
 @admin.register(Rol)
 class RolAdmin(admin.ModelAdmin):
-    list_display = ("id", 'employee','client','boss','shop','barn',)
+    list_display = ("id", 'employee','client','is_boss','shop','barn',)
     search_fields = (
         "employee__last_name",
         "employee__first_name",
