8ae41db46d5a9d2846acf53361ef2c205e350f67
Ikromov Isroil <ikromovisroil0309@gmail.com>
2026-04-01 15:10:12 +0500

    1

--- config/settings.py
+++ config/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / '.env')
 
 SECRET_KEY = os.environ["SECRET_KEY"]
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = [
     "report.imv.uz",
