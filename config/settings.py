f7aee19b6c18d724794eae7a3b694a5c09b555a0
Ikromov Isroil <ikromovisroil0309@gmail.com>
2026-04-03 12:07:29 +0500

    1

--- config/settings.py
+++ config/settings.py
@@ -282,22 +282,6 @@ else:
 
 CORS_ALLOW_CREDENTIALS = True
 
-
-# =========================================================
-# EMAIL
-# =========================================================
-EMAIL_BACKEND = os.getenv(
-    "EMAIL_BACKEND",
-    "django.core.mail.backends.smtp.EmailBackend"
-)
-EMAIL_HOST = os.getenv("EMAIL_HOST", "")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
-EMAIL_USE_TLS = env_bool("EMAIL_USE_TLS", True)
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "webmaster@localhost")
-
-
 # =========================================================
 # SSO
 # =========================================================
