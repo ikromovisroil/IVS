45833035435bc5f29dddef97e204ed1c897541f8
Ikromov Isroil <110326229+ikromovisroil@users.noreply.github.com>
2026-09-07 18:01:40 +0500

    1

--- main/sanitizers.py
+++ main/sanitizers.py
@@ -5,11 +5,23 @@ from bleach.css_sanitizer import CSSSanitizer
 ALLOWED_DEED_TAGS = [
     "div", "span", "b", "u", "i", "strong", "em", "br", "p",
     "table", "colgroup", "col", "tbody", "thead", "tr", "td", "th",
+    "s", "strike", "del",
+    "sup", "sub",
+    "code", "pre",
+    "h1", "h2", "h3", "h4", "h5", "h6",
+    "blockquote",
+    "ul", "ol", "li",
+    "a",
+    "img",
+    "hr",
 ]
+
 ALLOWED_DEED_ATTRS = {
     "*": ["style", "id", "class", "colspan", "rowspan"],
     "col": ["style"],
     "table": ["style", "border"],
+    "a": ["href", "target", "rel"],
+    "img": ["src", "alt", "width", "height"],
 }
 
 ALLOWED_DEED_STYLES = [
@@ -17,9 +29,9 @@ ALLOWED_DEED_STYLES = [
     "background-color", "color", "font-weight", "font-style",
     "page-break-after", "page-break-before", "page-break-inside",
     "break-after", "break-before", "break-inside",
-    "line-height",
+    "line-height", "font-family",
+    "text-indent", "margin-left", "margin-right",
 ]
-
 
 def sanitize_deed_body(raw_html):
     cleaner = bleach.sanitizer.Cleaner(
@@ -29,5 +41,6 @@ def sanitize_deed_body(raw_html):
             allowed_css_properties=ALLOWED_DEED_STYLES
         ),
         strip=True,
+        protocols=["http", "https", "mailto"],
     )
     return cleaner.clean(raw_html)
